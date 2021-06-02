738c52f002f193d628d2b1741e3e7019fffdf97d
smendoza07 <mendozasaul07@gmail.com>
2021-06-02 00:02:10 -0700

    Paddles and ball kind of

--- source/main.c
+++ source/main.c
@@ -1,23 +1,243 @@
-/*	Author: lab
+/*	Author: Saul Mendoza
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 22
+ *	Assignment: Lab #10  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Demo Link: 
+ *
  */
+
 #include <avr/io.h>
+#include "timer.h"
+#include "scheduler.h"
+#include "bit.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+//-----------Gobal Declarations---------
+unsigned char ball_row = 0x00;
+unsigned char ball_column = 0x00;
+unsigned char P1_column = 0x00;
+unsigned char P1_row = 0x00;
+unsigned char P2_column = 0x00;
+unsigned char P2_row = 0x00;
+//-----------end Globals----------------
 
-    /* Insert your solution below */
-    while (1) {
+//--------------------------------------
+// LED Matrix Demo SynchSM
+// Period: 100 ms
+//--------------------------------------
+enum Ball_States {shift_right, shift_left};
 
-    }
-    return 1;
+int Ball_Movement(int state) {
+
+	// Local Variables
+	static unsigned char pattern = 0x80;	// LED pattern - 0: LED off; 1: LED on
+	static unsigned char row = 0xFB;  	// Row(s) displaying pattern. 
+							// 0: display pattern on row
+							// 1: do NOT display pattern on row
+	// Transitions
+	switch (state) {
+		case shift_right:	
+			break;
+		case shift_left:
+			break;
+		default:	
+			state = shift_right;
+			break;
+	}	
+	// Actions
+	switch (state) {
+		case shift_right:	
+			if (pattern == 0x01) { // Reset demo 
+				state = shift_left;
+			} else { // Shift LED one spot to the right on current row
+				pattern >>= 1;
+			}
+			break;
+		case shift_left:
+			if (pattern == 0x80)
+				state = shift_right;
+			else
+				pattern <<= 1;
+			break;
+		default:
+			break;
+	}
+	ball_column = pattern;	// Pattern to display
+	//ball_row = row;		// Row(s) displaying pattern
+	PORTD = row;	
+	return state;
+}
+
+enum P1_States { P1_Wait, P1_MoveUp, P1_MoveDown };
+
+int P1_Movement (int state) {
+	static unsigned char column = 0x80;
+	static unsigned char rows = 0x00;
+	static unsigned char i = 0;
+	
+	switch (state) {
+		case P1_Wait:
+			state = P1_MoveDown;
+			i = 0;
+			break;
+		case P1_MoveDown:
+			if(i < 7 ){
+				state = P1_MoveDown;
+				i++;
+			}
+			else
+				state = P1_Wait;
+			break;
+		default:
+			state = P1_Wait;
+			break;
+	}
+	
+	switch (state) {
+		case P1_Wait:
+			rows = 0x00;
+			break;
+		case P1_MoveDown:
+			rows = 0xFF;
+			break;
+	}
+			
+	
+	P1_column = column;
+	//P1_row = rows;
+	PORTD = rows;
+	return state;
+}
+
+enum P2_States { P2_Wait, P2_MoveUp, P2_MoveDown };
+
+int P2_Movement (int state) {
+	static unsigned char column = 0x01;
+	static unsigned char rows = 0x00;
+	static unsigned char i = 0;
+	
+	switch (state) {
+		case P2_Wait:
+			state = P2_MoveDown;
+			i = 0;
+			break;
+		case P2_MoveDown:
+			if(i < 7){
+				state = P2_MoveDown;
+				i++;
+			}
+			else
+				state = P2_Wait;
+			break;
+		default:
+			state = P2_Wait;
+			break;
+	}
+	
+	switch (state) {
+		case P2_Wait:
+			rows = 0x00;
+			break;
+		case P2_MoveDown:
+			rows = 0xFF;
+			break;
+	}
+	
+	P2_column = column;
+	//P2_row = rows;
+	PORTD = rows;
+	return state;
+}
+
+enum display_States { display };
+
+// Combine outputs from SM's, and output on PORTB
+int displaySMTick(int state) {
+	// Local Variables
+	static unsigned char Column_output;
+	static unsigned char Row_output;
+	
+	switch (state) { //State machine transitions
+		case display: 
+			state = display; 
+			break;
+		default: 
+			state = display; 
+			break;
+	}
+	switch(state) { //State machine actions
+		case display:	
+			Column_output = P1_column | P2_column | ball_column; // write shared outputs
+			//Row_output = P1_row & P2_row & ball_row;	// to local variables
+		break;
+	}
+	PORTC = Column_output;
+	//PORTD = Row_output;// Write combined, shared output variables to PORTB
+	return state;
+}
+
+			
+	
+int main() {
+	DDRC = 0xFF; PORTC = 0x00; // PORTB set to output, outputs init 0s
+	DDRD = 0xFF; PORTD = 0x00; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
+
+	//Declare an array of tasks 
+	static task task1, task2, task3, task4;
+	task *tasks[] = { &task1, &task2, &task3, &task4 };
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+	const char start = 0;
+	// Task 1 (Ball_Movement)
+	task1.state = start;//Task initial state.
+	task1.period = 100;//Task Period.
+	task1.elapsedTime = task1.period;//Task current elapsed time.
+	task1.TickFct = &Ball_Movement;//Function pointer for the tick.
+	// Task 2 (P1_Movement)
+	task2.state = start;//Task initial state.
+	task2.period = 1;//Task Period.
+	task2.elapsedTime = task2.period;//Task current elapsed time.
+	task2.TickFct = &P1_Movement;//Function pointer for the tick.
+	// Task 3 (P1_Movement)
+	task3.state = start;//Task initial state.
+	task3.period = 1;//Task Period.
+	task3.elapsedTime = task3.period;//Task current elapsed time.
+	task3.TickFct = &P2_Movement;//Function pointer for the tick.
+	// Task 4 (displaySMTick)
+	task4.state = start;//Task initial state.
+	task4.period = 1;//Task Period.
+	task4.elapsedTime = task4.period;//Task current elapsed time.
+	task4.TickFct = &displaySMTick;//Function pointer for the tick.
+	
+	unsigned short j;
+	unsigned long GCD = tasks[0]->period;
+	for ( j = 1; j < numTasks; j++ ) {
+		GCD = findGCD(GCD,tasks[j]->period);
+	}	
+
+
+	// Set the timer and turn it on
+	TimerSet(GCD);
+	TimerOn();
+
+	unsigned short i; // Scheduler for-loop iterator
+	while(1) {	
+		for ( i = 0; i < numTasks; i++ ) { // Scheduler code
+			if ( tasks[i]->elapsedTime == tasks[i]->period ) { // Task is ready to tick
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state); // Set next state 
+				tasks[i]->elapsedTime = 0; // Reset the elapsed time for next tick.
+			}
+			tasks[i]->elapsedTime += GCD;
+		}
+		while(!TimerFlag);
+			TimerFlag = 0;
+	}
+	return 0; // Error: Program should not exit!
 }
